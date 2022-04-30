41d88103a244c47eeb96333486525bd3072accdf
Yuliia Kramarenko <ykramarenko@swivl.com>
2022-04-30 15:01:18 +0300

    Initial commit

---
+++ src/Homework2.java
@@ -0,0 +1,19 @@
+public class Homework2 {
+    public static void main(String[] args) {
+//        Завдання 1
+        int a = 5;
+        int b = 10;
+        int c = 3;
+        int volume = a * b * c;
+        int length = a + b + c;
+
+        System.out.println("Обʼєм паралелепіпеда = " + volume);
+        System.out.println("Сума всіх сторін = " + length);
+
+//        Завдання 2
+        double latitude = 48.40361286187686;
+        double longitude = 35.03988729614516;
+
+        System.out.println("Місце положення: " + latitude + ", " + longitude);
+    }
+}
